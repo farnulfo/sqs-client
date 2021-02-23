3c16d62f1759703db7817892d2d0e4e6c1517055
Franck Arnulfo <franck.arnulfo@gmail.com>
2021-02-23 12:12:32 +0000

    Update

--- src/main/java/org/arnulfo/Decrypt.java
+++ src/main/java/org/arnulfo/Decrypt.java
@@ -14,9 +14,21 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Decrypt 
 {
+    public static final int AES_KEY_LENGTH = 256;
+    public static final int GCM_AAD_LENGTH = 16;
+    public static final int GCM_TAG_LENGTH = 16;
+    public static final int GCM_NONCE_LENGTH = 12;
+
     public static void main( String[] args ) throws Exception {
         String cryptographyKey = System.getenv().get("CRYPTO_KEY");
         System.out.println(cryptographyKey);
@@ -29,5 +41,43 @@ public class Decrypt
         String body = Files.readAllLines(path).get(0);
         System.out.println("Body:");
         System.out.println(body);
+
+        // Decode Base64 body
+        byte[] binaryBody = Base64.getDecoder().decode(body);
+
+        // Remove Tag from the cipher text
+        // First retrieve Tag bytes
+        byte[] tag =  Arrays.copyOf(binaryBody, GCM_TAG_LENGTH);
+        //byte[] tag =  Arrays.copyOfRange(binaryBody, binaryBody.length - GCM_TAG_LENGTH, binaryBody.length);
+
+        // Remove Tag from the binary body to have the cipher text
+        byte[] cipherText = Arrays.copyOfRange(binaryBody, GCM_TAG_LENGTH, binaryBody.length);
+        //byte[] cipherText =  Arrays.copyOf(binaryBody, binaryBody.length);
+        //byte[] cipherText = Arrays.copyOfRange(binaryBody, 0, binaryBody.length - GCM_TAG_LENGTH);
+
+        // Prepare AAD by copying the first 16 bytes from cipher text.
+        byte[] aad = Arrays.copyOf(cipherText, GCM_AAD_LENGTH);
+        System.out.println("aad.length=" + aad.length);
+
+        // Prepare NONCE (IV) by copying bytes 4 – 16 from AAD. 
+        byte[] iv = Arrays.copyOfRange(aad, 4, 16);
+        System.out.println("iv.length=" + iv.length);
+
+        //
+        SecretKeySpec secretKeySpec = new SecretKeySpec(cypherKey, "AES");
+        
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, iv);
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, gcmParameterSpec);
+        cipher.updateAAD(aad);
+
+        byte[] decryptedText = cipher.doFinal(cipherText);
+
+        String plainText = new String(decryptedText);
+
+        System.out.println("Plain Text:");
+        System.out.println(plainText);
+
     }
 }
