b804e7e55097a9c82e337a4ebf0a970887613c79
Franck Arnulfo <franck.arnulfo@gmail.com>
2021-02-23 12:20:16 +0000

    Update

--- src/main/java/org/arnulfo/App.java
+++ src/main/java/org/arnulfo/App.java
@@ -10,6 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
+        // Region : US_EAST_1 based on the  "https://sqs.us-east-1.amazonaws.com/"
+        // Use of EnvironmentVariableCredentialsProvider :
+        // an AwsCredentialsProvider implementation that loads credentials
+        // from the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SESSION_TOKEN environment variables.
+        // Need to make :
+        // export AWS_ACCESS_KEY_ID="XXXXXXXXXXXXXXX"
+        // export AWS_SECRET_ACCESS_KEY="YYYYYYY"
+        // export AWS_SESSION_TOKEN="WWWWWWWWW"
+        // AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN : from https://api.refinitiv.com/auth/cloud-credentials/v1/?endpoint= 
+        
         SqsClient sqsClient = SqsClient.builder()
         .region(Region.US_EAST_1)
         .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
