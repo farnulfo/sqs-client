b57799d624dddaa3c3b8adcbc01d1b65ddcf97d8
Franck Arnulfo <franck.arnulfo@gmail.com>
2021-02-23 11:00:52 +0000

    Update

--- src/main/java/org/arnulfo/Decrypt.java
+++ src/main/java/org/arnulfo/Decrypt.java
@@ -1,10 +1,23 @@
 package org.arnulfo;
 
 import java.util.Base64;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Decrypt 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
         String cryptographyKey = System.getenv().get("CRYPTO_KEY");
         System.out.println(cryptographyKey);
 
@@ -12,8 +25,9 @@ public class Decrypt
         byte[] cypherKey = Base64.getDecoder().decode(cryptographyKey);
 
         // Load body
-        Path path = Paths.get("src/test/resources/fileTest.txt");
-
-        String read = Files.readAllLines(path).get(0);
+        Path path = Paths.get("/home/codespace/workspace/sqs-client/body.txt");
+        String body = Files.readAllLines(path).get(0);
+        System.out.println("Body:");
+        System.out.println(body);
     }
 }
